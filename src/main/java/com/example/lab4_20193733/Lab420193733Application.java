b5c6bbb73fd7d84ba0813a8927bb35c792c7d3bd
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-07 20:38:52 -0500

    Empezamos el laboratorio

---
+++ src/main/java/com/example/lab4_20193733/Lab420193733Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20193733;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420193733Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420193733Application.class, args);
+	}
+
+}
